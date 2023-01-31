f945a3c7f91831ef14a919b2011af07cba19d4a6
VAlmea <almeavicente@gmail.com>
2023-01-31 18:11:58 +0100

    Create ItemViewModel.cs

---
+++ generators/spa/templates/common/ViewModels/ItemViewModel.cs
@@ -0,0 +1,41 @@
+﻿using <%= fullNamespace %>.Components;
+using <%= fullNamespace %>.Data;
+using Newtonsoft.Json;
+
+namespace <%= fullNamespace %>.Services.ViewModels
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ItemViewModel
+    {
+        public ItemViewModel(Item t)
+        {
+            Id = t.ItemId;
+            Name = t.ItemName;
+            Description = t.ItemDescription;
+            AssignedUser = t.AssignedUserId;
+        }
+
+        public ItemViewModel(Item t, bool canEdit) : this(t)
+        {
+            CanEdit = canEdit;
+        }
+
+
+        public ItemViewModel() { }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("assignedUser")]
+        public int? AssignedUser { get; set; }
+
+        [JsonProperty("canEdit")]
+        public bool CanEdit { get; }
+    }
+}
