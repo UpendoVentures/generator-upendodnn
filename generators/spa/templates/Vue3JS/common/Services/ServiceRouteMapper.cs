a7d6521a2b494773528fd2d6ef0cf9a6dbc822e0
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-10-30 11:22:21 -0400

    Create ServiceRouteMapper.cs

---
+++ generators/spa/templates/Vue3JS/common/Services/ServiceRouteMapper.cs
@@ -0,0 +1,24 @@
+﻿using DotNetNuke.Web.Api;
+using System.Web.Http;
+
+namespace <%= fullNamespace %>.Services
+{
+    /// <summary>
+    /// The ServiceRouteMapper tells the DNN Web API Framework what routes this module uses
+    /// </summary>
+    public class ServiceRouteMapper : IServiceRouteMapper
+    {
+         /// <summary>
+        /// RegisterRoutes is used to register the module's routes
+        /// </summary>
+        /// <param name="mapRouteManager"></param>
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute(
+                moduleFolderName: "<%= fullNamespace %>",
+                routeName: "default",
+                url: "{controller}/{action}",
+                namespaces: new[] { "<%= fullNamespace %>.Services" });
+        }
+    }
+}
