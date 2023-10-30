931a46bc8a7ccc3c34f014ef55fc9456d3649621
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-10-30 11:16:58 -0400

    Create HomeItem.vue

---
+++ generators/spa/templates/Vue3JS/common/client-app/src/components/HomeItem.vue
@@ -0,0 +1,21 @@
+﻿<template>
+    <div class="text-center">
+        <h2>{{ resx.Welcome }}</h2>
+    </div>
+</template>
+
+<script setup>
+import { inject, } from 'vue';
+const resx = inject("resx");
+</script>
+
+<script>
+export default {
+    components: {},
+    data() {
+        return {};
+    },
+    mounted() { },
+    methods: {},
+};
+</script>
