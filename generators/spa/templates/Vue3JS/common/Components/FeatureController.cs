1e493b9b7df2f262ae61e498387473bcd43abe8b
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-10-31 16:00:54 -0400

    Update FeatureController.cs

--- generators/spa/templates/Vue3JS/common/Components/FeatureController.cs
+++ generators/spa/templates/Vue3JS/common/Components/FeatureController.cs
@@ -1,20 +1,25 @@
+
+using DotNetNuke.Common;
+using DotNetNuke.Common.Utilities;
 using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Hosting;
+using System.Xml;
 
 namespace <%= fullNamespace %>.Components
 {
     /// <summary>
     /// The controller class responsible for managing module features and upgrades.
     /// </summary>
-    public sealed class FeatureController : IUpgradeable
+    public sealed class FeatureController
     {
-        /// <summary>
-        /// Handles module upgrades for the specified version.
-        /// </summary>
-        /// <param name="Version">The version to which the module is being upgraded.</param>
-        /// <returns>A message indicating the result of the upgrade process.</returns>
-        public string UpgradeModule(string Version)
-        {
-            return "Success";
-        }
     }
 }
