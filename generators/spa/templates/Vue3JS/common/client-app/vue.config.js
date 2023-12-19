84fe76c28de132da34f31fb8100874fd5cde259e
William Shelton <WJShelton@CCISD.US>
2023-12-19 09:55:51 -0600

    Update vue.config.js Fix for bug: Calling incorrect URL when loading JavaScrpt Chucks

--- generators/spa/templates/Vue3JS/common/client-app/vue.config.js
+++ generators/spa/templates/Vue3JS/common/client-app/vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "../Scripts/client-app/",
+  publicPath: "/DesktopModules/<%= friendlyName %>/Scripts/client-app/",
   filenameHashing: false
 })
