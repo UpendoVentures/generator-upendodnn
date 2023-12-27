e1cf5ccaf20e14edf9a8a29525595d62902c6042
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-12-27 10:27:39 -0500

    Update router.js
    Update with an example of the router with correct use in DNN.

--- generators/spa/templates/Vue3JS/common/client-app/src/router.js
+++ generators/spa/templates/Vue3JS/common/client-app/src/router.js
@@ -1,12 +1,17 @@
 ﻿import { createRouter, createWebHistory } from 'vue-router';
 import HomeItem from './components/HomeItem.vue';
+import ItemDetails from './components/ItemDetails.vue';
+import About from './components/AboutItem.vue';
+import { resolveHomePath, resolvePath } from './assets/utils'
 
 const url = new URL(window.location.href);
 const basePath = url.pathname;
 
 const routes = [
-    { path: `${basePath}`, component: HomeItem },
-    { path: '/:pathMatch(.*)*', redirect: `${basePath}` }
+    { path: resolveHomePath(basePath), component: HomeItem },
+    { path: resolvePath(basePath, '/details'), component: ItemDetails },
+    { path: resolvePath(basePath, '/about'), component: About },
+    { path: '/:pathMatch(.*)*', redirect: resolveHomePath(basePath) }
 ];
 
 const router = createRouter({
