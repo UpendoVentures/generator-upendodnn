9e998e634474d9c150232de628085113757aed0f
Alejandro Román Franco <alejoroman0605@gmail.com>
2023-12-27 10:24:34 -0500

    Create ItemDetails.vue

---
+++ generators/spa/templates/Vue3JS/common/client-app/src/components/ItemDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mx-3">
+        <div class="text-center">
+            <h2>{{ resx.DetailsView }}</h2>
+            <router-link :to="goHome()">{{ resx.Home }}</router-link>
+        </div>
+    </div>
+</template>
+<script setup>
+import { inject, } from 'vue';
+import { goHome } from '../assets/utils';
+
+// Injected dependencies
+const resx = inject("resx");
+</script>
